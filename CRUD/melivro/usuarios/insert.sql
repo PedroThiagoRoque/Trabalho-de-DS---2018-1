5542d70aafe6035c2cb676ffaebac16f77e40380
Marcos Saalfeld Bartz <msbartz@inf.ufpel.edu.br>
2018-06-16 14:57:40 -0300

    Delete insert.sql

--- CRUD/melivro/usuarios/insert.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO 'usuarios ('cidade', 'endereco', 'telefone', 'sexo', 'cpf') 
-VALUES ('Pelotas', 'Goncalves Chaves 1003', '32821525', 'M', '12345678901'),
-('Pelotas', 'Goncalves Chaves 1003', '32221525', 'F', '12345621901');
