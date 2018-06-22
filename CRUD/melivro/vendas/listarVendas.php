dea215df560657a72e54103f38c7e190602d1e4b
PedroThiagoRoque <pedronmroque@gmail.com>
2018-06-21 21:44:02 -0300

    fixed variaveis

--- CRUD/melivro/vendas/listarVendas.php
+++ CRUD/melivro/vendas/listarVendas.php
@@ -30,26 +30,26 @@
 	<table class="table">
 	<thead>
 		<tr>
-			<th class="table-secondary" scope="col">ID</th>
-			<th class="table-secondary" scope="col">Titulo</th>
-			<th class="table-secondary" scope="col">Preço</th>
-			<th class="table-secondary" scope="col">Descrição</th>
-			<th class="table-secondary" scope="col">Imagem</th>
-			<th class="table-secondary" scope="col">Opções</th>
+			<th class="table-secondary" scope="col">Código</th>
+			<th class="table-secondary" scope="col">Valor</th>
+			<th class="table-secondary" scope="col">Data</th>
+			<th class="table-secondary" scope="col">CPF Cliente</th>
+			<th class="table-secondary" scope="col">Código Pagamento</th>
+			
 		</tr>
 	</thead>
 	<tbody>
 	<?php if ($vendas) : ?>
 	<?php foreach ($vendas as $venda) : ?>	<tr>
-			<td><?php echo $venda["CODPROD"]; ?></td>
-			<td><?php echo $venda["TITULO"]; ?></td>
-			<td><?php echo $venda["PRECO"]; ?></td>
-			<td><?php echo $venda["DESCRICAO"]; ?></td>
-			<td><?php echo $venda["IMAGEM"]; ?></td>
+			<td><?php echo $venda["CODVENDA"]; ?></td>
+			<td><?php echo $venda["VALOR_VENDA"]; ?></td>
+			<td><?php echo $venda["DATA_VENDA"]; ?></td>
+			<td><?php echo $venda["CPF"]; ?></td>
+			<td><?php echo $venda["CODPAG"]; ?></td>
 				
 			<td class="table-secondary">
-				<a href="view.php?id=<?php echo $venda['CODPROD']; ?>" class="btn btn-sm btn-success"><i class="fa fa-eye"></i> Visualizar</a>
-				<a href="edit.php?id=<?php echo $venda['CODPROD']; ?>" class="btn btn-sm btn-warning"><i class="fa fa-pencil"></i> Editar</a>
+				<a href="view.php?id=<?php echo $venda['CODVENDA']; ?>" class="btn btn-sm btn-success"><i class="fa fa-eye"></i> Visualizar</a>
+				<a href="edit.php?id=<?php echo $venda['CODVENDA']; ?>" class="btn btn-sm btn-warning"><i class="fa fa-pencil"></i> Editar</a>
 				<a href="#" class="btn btn-sm btn-danger" data-toggle="modal" data-target="#delete-modal" data-venda="<?php echo $venda['CODPROD']; ?>">
 					<i class="fa fa-trash"></i> Excluir
 				</a>
