10dec80d7e2ec05bd1b81185c6460e2d62a019d2
Geovana <geovanasilvadasilveira1@gmail.com>
2018-06-25 22:03:10 -0300

    Update edit.php

--- CRUD/melivro/pagamento/edit.php
+++ CRUD/melivro/pagamento/edit.php
@@ -13,6 +13,10 @@
   <hr />
   <div class="row">
     <div class="form-group col-md-7">
+      <label for="name">ID</label>
+      <input type="text" class="form-control" name="pagamento['codpag']" value="<?php echo $pagamento['CODPAG'];?>" readonly>
+    </div>
+    <div class="form-group col-md-7">
       <label for="name">Metodo de Pagamento</label>
       <select type="text" class="form-control" placeholder="Metodo de Pagamento" name="pagamento['metodo_pag']">
          <option value="cartão">Cartão</option>
@@ -24,9 +28,12 @@
 
     <div class="form-group col-md-4">
       <label for="campo2">Valor</label>
-      <input type="number" class="form-control" name="pagamento['valor']" placeholder="Valor" min="1" value="<?php echo $pagamento['VALOR'];?>">
+      <input type="number" class="form-control" name="pagamento['valor']" placeholder="Valor" min="1" step=".01" value="<?php echo $pagamento['VALOR'];?>"readonly>
     </div>
-
+     <div class="form-group col-md-7">
+      <label for="campo1">Concretizado</label>
+      <input type="text" class="form-control" name="pagamento['concretizado']" value="<?php echo $pagamento['CONCRETIZADO'];?>"readonly>
+    </div>
   </div>
   <div class="row">
     
